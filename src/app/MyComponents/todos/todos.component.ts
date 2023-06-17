5a8f1008da1a177d18b03fd45068a1a8321b4573
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-06-17 17:20:18 +0530

    creating-todos-component

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent {
-
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: 'This is title',
+        desc: 'Description',
+        active: true,
+      },
+      {
+        sno: 2,
+        title: 'This is title2',
+        desc: 'Description',
+        active: true,
+      },
+      {
+        sno: 3,
+        title: 'This is title3',
+        desc: 'Description',
+        active: true,
+      },
+    ];
+  }
 }
